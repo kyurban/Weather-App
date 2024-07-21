d39ef8818f5cb5b5a0c1634da8c7a71339f63062
kurban15 <kyleurban99@gmail.com>
2024-07-21 16:31:51 -0700

    added CSS and better functionality

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 import requests
+import datetime
+from decouple import config
 
 # Create your views here.
 def index(request):
 
-    appid = '7c2e049482a114ec7cfa4bbcf5ee1c7f'
+    if 'city' in request.POST and request.POST['city']:
+        city = request.POST['city']
+    else:
+        city = 'Los Angeles'
+
+    appid = config('API_KEY')
     URL = 'http://api.openweathermap.org/data/2.5/weather'
-    PARAMS = {'q': 'Los Angeles', 'appid':appid, 'units':'metric'}
+    PARAMS = {'q': city, 'appid':appid, 'units':'metric'}
 
     r = requests.get(url=URL, params=PARAMS)
     data = r.json()
 
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
-
-    return render(request, 'index.html', {'description': description, 'icon': icon, 'temp': temp})
+    context = {
+        'description': data['weather'][0]['description'].title,
+        'icon': data['weather'][0]['icon'],
+        'temp': (int(data['main']['temp'] * 2)) + 30,
+        'humidity': data['main']['humidity'],
+        'city': city.title,
+        'day': datetime.date.today()
+     }
+    return render(request, 'index.html', context)
--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kcqx^#1rzws-y*g4r!iacmvw@zv5hl-gelh@d!$h8h2-n)b#w6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR, 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
